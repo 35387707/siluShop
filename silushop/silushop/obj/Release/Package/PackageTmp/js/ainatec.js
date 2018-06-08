4083300b74032078ca5749f9abfe9ad12f6f7d75
huangjinpeng8@hotmail.com <huangjinpeng8@hotmail.com>
2018-06-08 17:25:30 +0800

    去掉不必要的文件

--- silushop/silushop/obj/Release/Package/PackageTmp/js/ainatec.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){ 
-	$("#helptab>ul>li").click(function(){
-		index = $(this).index();
-		$("#helptab li").removeClass("active").eq(index).addClass("active");
-		$("#helptab_con ul").removeClass("active").eq(index).addClass("active");
-	})
-}); 
-;
